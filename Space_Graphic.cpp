275d6e630990df2fe01447ab303905d80adead21
RavenKZP <mkruk96@gmail.com>
2020-02-10 19:18:40 +0100

    Add Images

--- Space_Graphic.cpp
+++ Space_Graphic.cpp
@@ -8,6 +8,7 @@
 
 #define PI 3.141592653589793238462643383279502884197169
 #define TILE_SIZE 64
+#define IMAGE_SIZE 16
 
 int Ship_Rotate(std::vector<Ship_Class> Ships, int y, int x);
 
@@ -15,12 +16,11 @@ int seed;
 
 ALLEGRO_DISPLAY *display = NULL;
 
-ALLEGRO_BITMAP *Space_1 = NULL;
-ALLEGRO_BITMAP *Space_2 = NULL;
-ALLEGRO_BITMAP *Space_3 = NULL;
-ALLEGRO_BITMAP *Space_4 = NULL;
-ALLEGRO_BITMAP *Space_5 = NULL;
-ALLEGRO_BITMAP *Space_6 = NULL;
+ALLEGRO_BITMAP *Space_Background_1 = NULL;
+ALLEGRO_BITMAP *Space_Background_2 = NULL;
+ALLEGRO_BITMAP *Space_Galayx_1 = NULL;
+ALLEGRO_BITMAP *Space_Galayx_2 = NULL;
+ALLEGRO_BITMAP *Space_Galayx_3 = NULL;
 ALLEGRO_BITMAP *Asteroid_1 = NULL;
 ALLEGRO_BITMAP *Asteroid_2 = NULL;
 ALLEGRO_BITMAP *Asteroid_3 = NULL;
@@ -46,12 +46,11 @@ void Create_Display(int MAP_SIZE)
 	al_init_image_addon();
 	display = al_create_display(MAP_SIZE*TILE_SIZE, MAP_SIZE*TILE_SIZE);
 
-	Space_1 = al_load_bitmap("Images/Space_1.png");
-	Space_2 = al_load_bitmap("Images/Space_2.png");
-	Space_3 = al_load_bitmap("Images/Space_3.png");
-	Space_4 = al_load_bitmap("Images/Space_4.png");
-	Space_5 = al_load_bitmap("Images/Space_5.png");
-	Space_6 = al_load_bitmap("Images/Space_6.png");
+	Space_Background_1 = al_load_bitmap("Images/Space_Background_1.jpg");
+	Space_Background_2 = al_load_bitmap("Images/Space_Background_2.jpg");
+	Space_Galaxy_1 = al_load_bitmap("Images/Space_Galaxy_1");
+	Space_Galaxy_2 = al_load_bitmap("Images/Space_Galaxy_2");
+	Space_Galaxy_3 = al_load_bitmap("Images/Space_Galaxy_3");
 	Asteroid_1 = al_load_bitmap("Images/Asteroid_1.png");
 	Asteroid_2 = al_load_bitmap("Images/Asteroid_2.png");
 	Asteroid_3 = al_load_bitmap("Images/Asteroid_3.png");
@@ -88,80 +87,80 @@ void Draw_Bitmap(std::vector<std::vector<char> > MAP, std::vector<Ship_Class> Sh
 				case '.':
 					if (Space_Type == 1)
 					{
-						al_draw_scaled_rotated_bitmap(Space_1, 8, 8,i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/16, TILE_SIZE/16,  rotate, 0);
+						al_draw_scaled_rotated_bitmap(Space_1, IMAGE_SIZE/2, IMAGE_SIZE/2, i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/IMAGE_SIZE, TILE_SIZE/IMAGE_SIZE,  rotate, 0);
 					}
 					if (Space_Type == 2)
 					{
-						al_draw_scaled_rotated_bitmap(Space_2, 8, 8,i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/16, TILE_SIZE/16,  rotate, 0);
+						al_draw_scaled_rotated_bitmap(Space_2, IMAGE_SIZE/2, IMAGE_SIZE/2, i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/IMAGE_SIZE, TILE_SIZE/IMAGE_SIZE,  rotate, 0);
 					}
 					if (Space_Type == 3)
 					{
-						al_draw_scaled_rotated_bitmap(Space_3, 8, 8,i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/16, TILE_SIZE/16,  rotate, 0);
+						al_draw_scaled_rotated_bitmap(Space_3, IMAGE_SIZE/2, IMAGE_SIZE/2, i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/IMAGE_SIZE, TILE_SIZE/IMAGE_SIZE,  rotate, 0);
 					}
 					if (Space_Type == 4)
 					{
-						al_draw_scaled_rotated_bitmap(Space_4, 8, 8,i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/16, TILE_SIZE/16,  rotate, 0);
+						al_draw_scaled_rotated_bitmap(Space_4, IMAGE_SIZE/2, IMAGE_SIZE/2, i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/IMAGE_SIZE, TILE_SIZE/IMAGE_SIZE,  rotate, 0);
 					}
 					if (Space_Type == 5)
 					{
-						al_draw_scaled_rotated_bitmap(Space_5, 8, 8,i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/16, TILE_SIZE/16,  rotate, 0);
+						al_draw_scaled_rotated_bitmap(Space_5, IMAGE_SIZE/2, IMAGE_SIZE/2, i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/IMAGE_SIZE, TILE_SIZE/IMAGE_SIZE,  rotate, 0);
 					}
 					if (Space_Type == 6)
 					{
-						al_draw_scaled_rotated_bitmap(Space_6, 8, 8,i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/16, TILE_SIZE/16,  rotate, 0);
+						al_draw_scaled_rotated_bitmap(Space_6, IMAGE_SIZE/2, IMAGE_SIZE/2, i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/IMAGE_SIZE, TILE_SIZE/IMAGE_SIZE,  rotate, 0);
 					}
 					break;
 				case 'A':
 					if (Asteroid_Type == 1)
 					{
-						al_draw_scaled_rotated_bitmap(Asteroid_1, 8, 8,i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/16, TILE_SIZE/16,  rotate, 0);
+						al_draw_scaled_rotated_bitmap(Asteroid_1, IMAGE_SIZE/2, IMAGE_SIZE/2, i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/IMAGE_SIZE, TILE_SIZE/IMAGE_SIZE,  rotate, 0);
 					}
 					if (Asteroid_Type == 2)
 					{
-						al_draw_scaled_rotated_bitmap(Asteroid_2, 8, 8,i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/16, TILE_SIZE/16,  rotate, 0);
+						al_draw_scaled_rotated_bitmap(Asteroid_2, IMAGE_SIZE/2, IMAGE_SIZE/2, i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/IMAGE_SIZE, TILE_SIZE/IMAGE_SIZE,  rotate, 0);
 					}
 					if (Asteroid_Type == 3)
 					{
-						al_draw_scaled_rotated_bitmap(Asteroid_3, 8, 8,i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/16, TILE_SIZE/16,  rotate, 0);
+						al_draw_scaled_rotated_bitmap(Asteroid_3, IMAGE_SIZE/2, IMAGE_SIZE/2, i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/IMAGE_SIZE, TILE_SIZE/IMAGE_SIZE,  rotate, 0);
 					}
 					break;
 				case 'M':
 					if (Mine_Type == 1)
 					{
-						al_draw_scaled_rotated_bitmap(Mine_1, 8, 8,i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/16, TILE_SIZE/16,  rotate, 0);
+						al_draw_scaled_rotated_bitmap(Mine_1, IMAGE_SIZE/2, IMAGE_SIZE/2, i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/IMAGE_SIZE, TILE_SIZE/IMAGE_SIZE,  rotate, 0);
 					}
 					if (Mine_Type == 2)
 					{
-						al_draw_scaled_rotated_bitmap(Mine_2, 8, 8,i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/16, TILE_SIZE/16,  rotate, 0);
+						al_draw_scaled_rotated_bitmap(Mine_2, IMAGE_SIZE/2, IMAGE_SIZE/2, i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/IMAGE_SIZE, TILE_SIZE/IMAGE_SIZE,  rotate, 0);
 					}
 					if (Mine_Type == 3)
 					{
-						al_draw_scaled_rotated_bitmap(Mine_3, 8, 8,i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/16, TILE_SIZE/16,  rotate, 0);
+						al_draw_scaled_rotated_bitmap(Mine_3, IMAGE_SIZE/2, IMAGE_SIZE/2, i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/IMAGE_SIZE, TILE_SIZE/IMAGE_SIZE,  rotate, 0);
 					}
 					break;
 				case 'W':
-					al_draw_scaled_rotated_bitmap(Workshop_1, 8, 8,i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/16, TILE_SIZE/16,  rotate, 0);
+					al_draw_scaled_rotated_bitmap(Workshop_1, IMAGE_SIZE/2, IMAGE_SIZE/2, i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/IMAGE_SIZE, TILE_SIZE/IMAGE_SIZE,  rotate, 0);
 					break;
 				case 'w':
-					al_draw_scaled_rotated_bitmap(Workshop_2, 8, 8,i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/16, TILE_SIZE/16,  rotate, 0);
+					al_draw_scaled_rotated_bitmap(Workshop_2, IMAGE_SIZE/2, IMAGE_SIZE/2, i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/IMAGE_SIZE, TILE_SIZE/IMAGE_SIZE,  rotate, 0);
 					break;
 				case 'H':
-					al_draw_scaled_rotated_bitmap(HQ_1, 8, 8,i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/16, TILE_SIZE/16,  rotate, 0);
+					al_draw_scaled_rotated_bitmap(HQ_1, IMAGE_SIZE/2, IMAGE_SIZE/2, i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/IMAGE_SIZE, TILE_SIZE/IMAGE_SIZE,  rotate, 0);
 					break;
 				case 'h':
-					al_draw_scaled_rotated_bitmap(HQ_2, 8, 8,i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/16, TILE_SIZE/16,  rotate, 0);
+					al_draw_scaled_rotated_bitmap(HQ_2, IMAGE_SIZE/2, IMAGE_SIZE/2, i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/IMAGE_SIZE, TILE_SIZE/IMAGE_SIZE,  rotate, 0);
 					break;
 				case 'F':
-					al_draw_scaled_rotated_bitmap(Fighter_1, 8, 8,i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/16, TILE_SIZE/16,  Ship_Rotate(Ships, i, j), 0);
+					al_draw_scaled_rotated_bitmap(Fighter_1, IMAGE_SIZE/2, IMAGE_SIZE/2, i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/IMAGE_SIZE, TILE_SIZE/IMAGE_SIZE,  Ship_Rotate(Ships, i, j), 0);
 					break;
 				case 'f':
-					al_draw_scaled_rotated_bitmap(Fighter_2, 8, 8,i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/16, TILE_SIZE/16,  Ship_Rotate(Ships, i, j), 0);
+					al_draw_scaled_rotated_bitmap(Fighter_2, IMAGE_SIZE/2, IMAGE_SIZE/2, i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/IMAGE_SIZE, TILE_SIZE/IMAGE_SIZE,  Ship_Rotate(Ships, i, j), 0);
 					break;
 				case 'T':
-					al_draw_scaled_rotated_bitmap(Transport_1, 8, 8,i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/16, TILE_SIZE/16,  Ship_Rotate(Ships, i, j), 0);
+					al_draw_scaled_rotated_bitmap(Transport_1, IMAGE_SIZE/2, IMAGE_SIZE/2, i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/IMAGE_SIZE, TILE_SIZE/IMAGE_SIZE,  Ship_Rotate(Ships, i, j), 0);
 					break;
 				case 't':
-					al_draw_scaled_rotated_bitmap(Transport_2, 8, 8,i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/16, TILE_SIZE/16,  Ship_Rotate(Ships, i, j), 0);
+					al_draw_scaled_rotated_bitmap(Transport_2, IMAGE_SIZE/2, IMAGE_SIZE/2, i*TILE_SIZE +TILE_SIZE/2, j*TILE_SIZE +TILE_SIZE/2, TILE_SIZE/IMAGE_SIZE, TILE_SIZE/IMAGE_SIZE,  Ship_Rotate(Ships, i, j), 0);
 					break;
 			}
 		}
